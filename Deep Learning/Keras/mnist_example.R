1dff1ce997bd5080978e9f51a8f0703b75ea975c
Yuum <rhawl9764@naver.com>
2019-05-30 22:26:20 +0900

    mnist

---
+++ Deep Learning/Keras/mnist_example.R
@@ -0,0 +1,78 @@
+library(keras)
+
+mnist <- dataset_mnist()
+
+train_images <- mnist$train$x
+train_labels <- mnist$train$y
+
+train_images <- array_reshape(train_images, c(60000, 28*28))
+train_images <- train_images/255 #rgb이므로 숫자를 최대한 낮게 하려고! 
+
+test_images <- mnist$test$x
+test_labels <- mnist$test$y
+test_images <- array_reshape(test_images, c(10000, 28*28))
+test_images <- test_images/255
+
+train_labels <- to_categorical(train_labels)
+test_labels <- to_categorical(test_labels)
+
+network <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", input_shape = c(28*28)) %>%
+  layer_dense(units = 10, activation = "softmax")
+
+
+network %>% compile(
+  optimizer = "rmsprop",
+  loss = "categorical_crossentropy",
+  metrics = c("accuracy")
+)
+
+network %>% fit(train_images, train_labels, epochs = 5, batch_size = 128)
+
+results.dnn <- network %>% evaluate(test_images, test_labels)
+results.dnn #97.8
+
+
+#-- cnn
+
+c(c(train_images, train_labels), c(test_images, test_labels)) %<-% mnist
+
+train_images <- array_reshape(train_images, c(60000, 28, 28, 1))
+train_images <- train_images / 255
+test_images <- array_reshape(test_images, c(10000, 28, 28, 1))
+test_images <- test_images / 255
+train_labels <- to_categorical(train_labels)
+test_labels <- to_categorical(test_labels)
+
+model <- keras_model_sequential() %>% 
+  layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
+                input_shape = c(28, 28, 1)) %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu")
+
+model
+
+model <- model %>% 
+  layer_flatten() %>% 
+  layer_dense(units = 64, activation = "relu") %>% 
+  layer_dense(units = 10, activation = "softmax")
+
+model
+
+model %>% compile(
+  optimizer = "rmsprop",
+  loss = "categorical_crossentropy",
+  metrics = c("accuracy")
+)
+
+model %>% fit(
+  train_images, train_labels, 
+  epochs = 5, batch_size=64
+)
+
+results.cnn <- model %>% evaluate(test_images, test_labels)
+results.cnn  #99.1로 상승 
+
+
